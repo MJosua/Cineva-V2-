ae8e45db992714899dacb44a11bce2712755a8b9
Muhammad Fariz Tamara Asmarakusuma <muhammad.asmarakusuma@icbp.indofood.co.id>
2025-01-06 18:20:45 +0700

    add index.js

--- index.js
+++ index.js
@@ -24,7 +24,6 @@
 // const purpleColor = '\x1b[35m'; // Purple
 // const reset = '\\x1b[0m';
 
-
 const express = require("express");
 const App = express();
 
@@ -42,8 +41,6 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-//TEST COMMIT AT farizBranch2
-
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -51,16 +48,15 @@ const cors = require("cors");
 
 const session = require("express-session");
 
+const os = require('os');
+
+//for production
+
+/*
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
-
-// const SSL = {
-//   key: fs.readFileSync(path.join(__dirname, process.env.SSL_LOC, process.env.SSL_TYPE, process.env.SSL_FILE_KEY)),
-//   cert: fs.readFileSync(path.join(__dirname, process.env.SSL_LOC, process.env.SSL_TYPE, process.env.SSL_FILE_CERT))
-// };
-
-// TEST PUSH
-
+ 
 
 const swaggerOptions = {
   definition: {
@@ -105,7 +101,49 @@ if (process.env.PORT === '9999') {
 //development
 // const svr = createServer(App);
 
-const PORT = process.env.PORT_SSL; // or any other port number you prefer
+// const PORT = process.env.PORT_SSL; // or any other port number you prefer
+*/
+
+
+//auto config
+
+
+//reading SSL certification directory
+const SSL = {
+  key: fs.readFileSync(path.join(__dirname, process.env.SSL_LOC, process.env.SSL_TYPE, process.env.SSL_FILE_KEY)),
+  cert: fs.readFileSync(path.join(__dirname, process.env.SSL_LOC, process.env.SSL_TYPE, process.env.SSL_FILE_CERT))
+};
+
+function production() {
+
+  function getLocalIp() {
+    const networkInterfaces = os.networkInterfaces();
+    for (const interfaceName in networkInterfaces) {
+      const addresses = networkInterfaces[interfaceName];
+      for (const address of addresses) {
+        if (address.family === 'IPv4' && !address.internal) {
+          return address.address; // Return the local IP address
+        }
+      }
+    }
+  }
+
+  if (getLocalIp() == "10.126.106.105") {
+    // return "production"
+    return true;
+  } else {
+    // return "development"
+    return false;
+  }
+
+
+} 
+let svr = production() ? https.createServer(SSL, App) : http.createServer(App);
+let PORT = production() ? process.env.PORT_SSL : process.env.PORT;
+console.log("Server status is Production?", production())
+
+
+
 
 const io = new Server(
   svr,
@@ -323,7 +361,6 @@ console.log(`INTEGRATED API running at Port: ${process.env.PORT}`);
 
 //TEST and DISPLAY APP
 App.get("/", (req, res) => {
-  console.log("Aktif")
   res
     .status(200)
     .send(
