cbeb142b30a01736c0d6cb5aff0d25353ca01935
Muhammad Fariz Tamara Asmarakusuma <muhammad.asmarakusuma@icbp.indofood.co.id>
2025-01-14 17:53:54 +0700

    aded getrRegion controller

--- controller/hotsTps.js
+++ controller/hotsTps.js
@@ -25,7 +25,7 @@ untuk utilitas pricing structure
 
 module.exports = {
 
-    
+
     getRegion: async (req, res) => {
 
 
@@ -33,14 +33,35 @@ module.exports = {
         let timestamp = date.toLocaleDateString('id') + ' ' + date.toLocaleTimeString('id') + ' : ' + ' ';
 
 
-        if (req.dataToken.user_id){
+        if (req.dataToken.user_id) {
+
+            let query = `SELECT region_id, region_desc FROM mst_region mr`
+
+            dbConf.execute(query, (err, results) => {
+
+                if (err) {
+                    res.status(500).send({
+                        success: false,
+                        message: `INTERNAL SERVER ERROR`
+                    });
+                    console.log(timestamp, "Error at getRegion, message:", err);
+                } else {
+                    res.status(500).send({
+                        success: true,
+                        message: `successfully get data region`,
+                        results
+                    });
+                    console.log(timestamp, "Error at getRegion, message:", err);
+                }
+
+            })
 
         } else {
             res.status(401).send({
                 success: false,
                 message: `Unauthorized`
             });
-            console.log(timestamp, "getCountry is Unauthorized");
+            console.log(timestamp, "getRegion is Unauthorized");
         }
 
     },
@@ -51,7 +72,7 @@ module.exports = {
         let timestamp = date.toLocaleDateString('id') + ' ' + date.toLocaleTimeString('id') + ' : ' + ' ';
 
 
-        if (req.dataToken.user_id){
+        if (req.dataToken.user_id) {
 
         } else {
             res.status(401).send({
@@ -67,7 +88,7 @@ module.exports = {
         let date = new Date();
         let timestamp = date.toLocaleDateString('id') + ' ' + date.toLocaleTimeString('id') + ' : ' + ' ';
 
-        if (req.dataToken.user_id){
+        if (req.dataToken.user_id) {
 
         } else {
             res.status(401).send({
@@ -84,7 +105,7 @@ module.exports = {
         let date = new Date();
         let timestamp = date.toLocaleDateString('id') + ' ' + date.toLocaleTimeString('id') + ' : ' + ' ';
 
-        if (req.dataToken.user_id){
+        if (req.dataToken.user_id) {
 
         } else {
             res.status(401).send({
