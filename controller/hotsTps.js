8fe6c83da123142c73fd7ff69a1b47571866cced
Muhammad Fariz Tamara Asmarakusuma <muhammad.asmarakusuma@icbp.indofood.co.id>
2025-01-15 20:07:49 +0700

    added getSKU middleware

--- controller/hotsTps.js
+++ controller/hotsTps.js
@@ -392,6 +392,130 @@ module.exports = {
 
         if (req.dataToken.user_id) {
 
+            let company_id = req.params.company_id
+
+            if (!company_id || company_id == 0) {
+
+                res.status(204).send({
+                    success: false,
+                    message: `company_id is not provided properly`
+                });
+
+                console.log(timestamp, "company_id is not provided properly");
+
+            } else {
+
+                let query = ` 	-- get SKU of distributor
+                            SELECT
+                            DISTINCT UPPER(CONCAT(COALESCE(mp.product_name_no, mp.product_name), " - ", mp.product_sku )) product_name_complete ,
+                            tc.txt country_name,
+                            mpc.product_type_name cat_name ,
+                            mc.company_name,
+                            mp.product_code,
+                            UPPER(mp.product_sku) product_sku,
+                            UPPER(COALESCE(mp.product_name_no, mp.product_name)) product_name,
+                            mp.ctn_height,
+                            mp.ctn_length,
+                            mp.ctn_width,
+                            mp.ctn_thick,
+                            mp.cont20,
+                            mp.cont40,
+                            mp.cont40hc,
+                            COALESCE(mi.moq, 0) moq ,
+                            COALESCE(mi.moq20, 0) moq20 ,
+                            (
+                            SELECT
+                                rate_unit
+                            FROM
+                                trs_so_detail
+                            WHERE
+                                client_id = mi.distributor_id
+                                AND company_id = mp.company_id
+                                AND sku_id = mp.product_code
+                            ORDER BY
+                                so_id DESC,
+                                version DESC
+                            LIMIT 1) rate_unit,
+                            (
+                            SELECT
+                                value
+                            FROM
+                                trs_so_detail
+                            WHERE
+                                client_id = mi.distributor_id
+                                AND company_id = mp.company_id
+                                AND sku_id = mp.product_code
+                            ORDER BY
+                                so_id DESC,
+                                version DESC
+                            LIMIT 1) price,
+                            link.img,
+                            link.order ,
+                            mb.brand_name,
+                            mp.net_weight,
+                            mp.per_carton ,
+                            mf.flavour_desc flavour_name,
+                            mp.tolling_id
+                        FROM
+                            map_item_for_dist mi
+                        LEFT JOIN mst_company mc ON
+                            mi.distributor_id = mc.company_id
+                        INNER JOIN mst_product mp ON
+                            mi.product_id = mp.product_id
+                            AND mp.company_id = 100
+                            AND mp.active = 1
+                        LEFT JOIN mst_country c ON
+                            mc.country_id = c.country_id
+                        LEFT JOIN sys_text tc ON
+                            c.country_name_id = tc.text_id
+                            AND tc.lang_id = 1
+                        LEFT JOIN m_product_link link ON
+                            mp.product_code = link.product_code
+                            AND flag = 1
+                        LEFT JOIN mst_brand mb ON
+                            mp.brand_id = mb.brand_id
+                            AND mp.company_id = mb.company_id
+                        LEFT JOIN mst_product_type mpc ON
+                            mp.product_type_id = mpc.product_type_id
+                            AND mp.division_id = mpc.division_id
+                        LEFT JOIN mst_flavour mf ON
+                            mf.flavour_id = mp.flavour_id
+                        WHERE
+                            now() BETWEEN mi.creation_date AND COALESCE(mi.finish_date, '9999-12-31')
+                            AND mi.distributor_id = ?
+                            AND mf.company_id = 100
+                            AND mp.tolling_id NOT IN (1, 6)
+                            AND COALESCE( mi.moq, 0 ) > 0
+                        ORDER BY
+                            mpc.product_type_name
+                            `
+
+                let parameter = [company_id]
+
+                dbConf.execute(query, parameter, (err, results) => {
+
+                    if (err) {
+
+                        res.status(500).send({
+                            success: false,
+                            message: `INTERNAL SERVER ERROR`
+                        });
+                        console.log(timestamp, "Error at getSKU, message:", err);
+
+                    } else {
+                        res.status(200).send({
+                            success: true,
+                            message: `successfully get data SKU`,
+                            results
+                        });
+                        console.log(timestamp, "successfully getSKU!");
+                    }
+
+                })
+
+            }
+
+
         } else {
             res.status(401).send({
                 success: false,
--- routers/hotsTps.js
+++ routers/hotsTps.js
@@ -9,6 +9,7 @@ route.get('/country/:employee_id', decodeTokenHT, hotsTps.getCountry);
 route.get('/analyst', decodeTokenHT, hotsTps.getAnaliyst);
 route.get('/distributor/:country_id', decodeTokenHT, hotsTps.getDistributor);
 route.get('/port/:company_id', decodeTokenHT, hotsTps.getPort);
+route.get('/sku/:company_id', decodeTokenHT, hotsTps.getSKU);
 
 
 
