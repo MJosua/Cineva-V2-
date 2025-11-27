26fa275a29c4b6b4eada8737876f2b4ad5c09a3c
MJosua <josua.prima@gmail.com>
2025-11-27 11:20:16 +0700

    all update

---
+++ service/automation/index.js
@@ -0,0 +1,11 @@
+const trademarkMgmtAuto = require('./trademarkMgmt')
+const notification = require('./notification')
+const cleanup = require('./cleanup')
+const automatemb = require('./meetingbook/automatemb')
+
+module.exports = {
+    trademarkMgmtAuto, 
+    notification,
+    cleanup,
+    automatemb
+};
